62a6b2299a67312ca331beb6d363ccbcfe842ce7
Solnyshkoo <kepetrova@edu.hse.ru>
2021-11-14 14:51:10 +0300

    Initial Commit

---
+++ kepetrovaPW5_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kepetrovaPW5_
+//
+//  Created by Ksenia Petrova on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
